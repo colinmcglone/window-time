d9d3e26145be8a14d72ffa81dc6be6c2557f99ac
Colin McGlone <me@colinmcglone.ca>
2016-01-09 08:12:07 -0800

    thanks dan

--- marketgrab/admin.py
+++ marketgrab/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Data, FiveDay, FiftyDay, TwoHundredDay
+from .models import Data, MovingAvg
 # Register your models here.
 
 admin.site.register(Data)
-admin.site.register(FiveDay)
-admin.site.register(FiftyDay)
-admin.site.register(TwoHundredDay)
+admin.site.register(MovingAvg)
--- marketgrab/models.py
+++ marketgrab/models.py
@@ -6,33 +6,20 @@ from django.db import models
 
 class Data(models.Model):
     ticker = models.CharField(max_length=10)
-    trading_day = models.CharField(max_length=10)
     volume = models.DecimalField(max_digits=15, decimal_places=0)
     open_price = models.DecimalField(max_digits=10, decimal_places=2)
     high_price = models.DecimalField(max_digits=10, decimal_places=2)
     low_price = models.DecimalField(max_digits=10, decimal_places=2)
     close_price = models.DecimalField(max_digits=10, decimal_places=2)
     aclose_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(editable=True, auto_now_add=False)
     def __str__(self):
         return self.ticker
 
-class FiveDay(models.Model):
+class MovingAvg(models.Model):
     ticker = models.CharField(max_length=10)
-    date = models.DateTimeField
+    date = models.DateField(editable=True, auto_now_add=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    def __str__(self):
-        return self.ticker
-
-class FiftyDay(models.Model):
-    ticker = models.CharField(max_length=10)
-    date = models.DateTimeField
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    def __str__(self):
-        return self.ticker
-
-class TwoHundredDay(models.Model):
-    ticker = models.CharField(max_length=10)
-    date = models.DateTimeField
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    span = models.IntegerField()
     def __str__(self):
         return self.ticker
