82605ab103b11a5ec6cee8453ccb55c1a5386a01
Colin McGlone <me@colinmcglone.ca>
2016-01-25 13:51:34 -0800

    Created detail view, got url matching working. Don't forget to 'pkill python' to see changes.

--- marketgrab/views.py
+++ marketgrab/views.py
@@ -5,4 +5,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the marketgrab index.")
+
+def detail(request, ticker):
+    return HttpResponse("You're looking at the details for %s." % ticker)
+
