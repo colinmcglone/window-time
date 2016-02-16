374ae7659e9228def1ee5fc7513711793eacf3b6
Colin McGlone <me@colinmcglone.ca>
2016-02-16 15:01:32 -0800

    views outputs dictionary for index.html template.

--- marketgrab/views.py
+++ marketgrab/views.py
@@ -1,35 +1,56 @@
 import os
 from django.conf import *
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render_to_response, render
 from django.http import HttpResponse
 from .models import Data, MovingAvg, Movements, Sigma
 from datetime import datetime
-
+from django.template import RequestContext
 
 def index(request):
-    markets = []
+
     ticker = Data.objects.values_list('ticker').distinct()
-    for index in ticker:
-        index = index[0]
-        market = Data.objects.filter(ticker=index).latest('date')
-        move = Movements.objects.filter(ticker=index, series='market').latest('date')
-        sig = Sigma.objects.filter(ticker=index, series='market').latest('date')
+    market = []
+    averages = []
+    for t in ticker:
+        t = t[0]
 
-        avgs = MovingAvg.objects.filter(ticker=index).values_list('span').distinct()
-        spans = []
-        for span in avgs:
-            span = span[0]
-            avgmove = Movements.objects.filter(ticker=index, series=span).latest('date')
-            avgsig = Sigma.objects.filter(ticker=index, series=span).latest('date')
-            spans.extend([span, avgmove.price, avgmove.percent, avgsig.value])
+        price = Data.objects.filter(ticker=t).latest('date').aclose_price
+        date = Data.objects.filter(ticker=t).latest('date').date
 
-        markets.extend([index, market.aclose_price, market.date, move.price, move.percent, sig.value, spans])
+        move_price = Movements.objects.filter(ticker=t, series='market').latest('date').price
+        move_percent = Movements.objects.filter(ticker=t, series='market').latest('date').percent
 
-        return HttpResponse("%s" % markets)
+        spans = MovingAvg.objects.values_list('span').distinct()
+        for s in spans:
+            s = s[0]
 
+            avg_price = Movements.objects.filter(ticker=t, series=s).latest('date').price
+            avg_percent = Movements.objects.filter(ticker=t, series=s).latest('date').percent
+            zscore = Movements.objects.filter(ticker=t, series=s).latest('date').zvalue
+
+            a = {
+                'ticker':t,
+                'span':s,
+                'price':avg_price,
+                'percent':avg_percent,
+                'zscore':zscore
+                }
+
+            averages.append(a)
+
+        i = {
+            'index':t,
+            'price':price,
+            'date':date,
+            'move_price':move_price,
+            'move_percent':move_percent
+            }
+
+        market.append(i)
+
+    context = RequestContext(request, {'market':market, 'averages':averages})
+
+    return render_to_response('marketgrab/index.html', context_instance = context)
 
 def detail(request, t):
     if Data.objects.filter(ticker=t).exists():
